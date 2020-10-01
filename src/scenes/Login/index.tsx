7630ebba469af0b700798369ff4d967a4b956cee
zausailov <46808438+zausailov@users.noreply.github.com>
2020-10-01 21:02:24 +0300

    feature/login screen (#9)
    * test push (#1) (#2)
    
    Co-authored-by: zausailov <46808438+zausailov@users.noreply.github.com>
    
    * started with login screen
    
    * add error boundary as example
    
    Co-authored-by: Stanislav <41422921+StanisPos@users.noreply.github.com>
    Co-authored-by: Stanislav Posidelov <sposidelov@roox.ru>

--- src/scenes/Login/index.tsx
+++ src/scenes/Login/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Button from '@components/button';
 import Form from './components/Form';
 
 const Login = () => {
-  return (
-    <div className="login">
-      <h1 className="login-logo">DISH PARSER</h1>
-      <Form />
-      <Button />
-    </div>
-  );
+	return (
+		<div className="login">
+			<h1 className="login-logo">DISH PARSER</h1>
+			<Form />
+		</div>
+	);
 };
 
 export default Login;
