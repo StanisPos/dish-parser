e5f4b6b84aa19acfc6f27a85b9cfb4d398df9ce6
Stanislav Posidelov <sposidelov@roox.ru>
2020-10-19 20:03:58 +0300

    add sort and update methods

--- mock-server/controllers/api/recipes/index.js
+++ mock-server/controllers/api/recipes/index.js
@@ -20,13 +20,41 @@ router.get('/', passport.authenticate('jwt', { session: false }), async (req, re
 
 router.get('/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
-    const item = await service.recipes.findOneById(req.body?.id);
+    const item = await service.recipes.findOneById(req.params?.id);
 
     res.status(200).json({
       item,
     });
   } catch (e) {
     res.status(400).json({
+      message: e.message || e,
+    });
+  }
+});
+
+router.put('/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const recipes = service.recipes.updateItem(req.params?.id, req.body);
+
+    res.status(200).json({
+      recipes,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: e.message || e,
+    });
+  }
+});
+
+router.put('/sort/:dir', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const recipes = service.recipes.sortListByType(req.params?.dir, req.query?.type);
+
+    res.status(200).json({
+      recipes,
+    });
+  } catch (e) {
+    res.status(500).json({
       message: e.message || e,
     });
   }
